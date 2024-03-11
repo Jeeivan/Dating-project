d9287953072e3d52db4af8d050c07a06c891dbb0
jeeivan <jeeivansri@gmail.com>
2024-03-11 15:22:29 +0000

    Added end points to front end to allow inputs to be sent to database

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,27 +1,67 @@
-import React from 'react'
+import { useState } from "react"
 
 export default function Form() {
+    const [formData, setFormData] = useState({
+        name: '',
+        gender: '',
+        age: '',
+        sexuality: ''
+    })
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = e.target
+        setFormData((prevFormData) => ({
+            ...prevFormData,
+            [name]: value
+        }))
+    }
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        try {
+            const response = await fetch(`http://localhost:3006/form/submit/`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    ...formData
+                }),
+            })
+
+            if (response.ok) {
+                console.log("Form submitted successfully");
+                window.location.href = '/'
+            } else {
+                console.log("Error submitting form");
+            }
+        } catch (error) {
+            console.error("Error submitting form:", error)
+        }
+    }
+    
   return (
     <div>
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>
                 What is your name?
-                <input type="text" name='name'/>
+                <input type="text" name='name' value={formData.name} onChange={handleChange}/>
             </label>
             <br />
             <label>
                 What is your gender?
-                <input type="text" name='gender'/>
+                <input type="text" name='gender'value={formData.gender} onChange={handleChange}/>
             </label>
             <br />
             <label>
                 What is your age?
-                <input type="number" name='age'/>
+                <input type="number" name='age' value={formData.age} onChange={handleChange}/>
             </label>
             <br />
             <label>
                 What is your sexuality?
-                <input type="text" name='sexulaity'/>
+                <input type="text" name='sexuality' value={formData.sexuality} onChange={handleChange}/>
             </label>
             <br />
             <button type='submit'>Submit</button>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,25 @@
 import Form from "../../components/Form/Form"
+import { useState } from "react"
 
 export default function Home() {
+    const [results, setResults] = useState([])
+
+async function fetchFormData() {
+    try {
+        const response = await fetch(`http://localhost:3006/form/display/`)
+        const data = await response.json()
+
+        if (response.ok) {
+            setResults(data)
+            console.log(data);
+        } else {
+            console.log('Failed to fetcg form data');
+        }
+    } catch (error) {
+        console.error("Error fetching form data", error)
+    }
+}
+
   return (
     <div>
         <div>Questionnaire</div>
